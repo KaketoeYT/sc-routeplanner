26d1387d4821790501ab92273b6159584529cc2a
KaketoeYT <Thijsderogee@gmail.com>
2026-03-17 08:49:31 +0100

    added parralel requesting

--- app/services/RoutesService.php
+++ app/services/RoutesService.php
@@ -2,18 +2,47 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Http;
+
 class routesService
 {
     protected $baseUrl = 'https://api.uexcorp.space/2.0';
 
     public function getRoutes()
     {
-        $url = $this->baseUrl . '/commodities_routes?id_commodity=1';
-        $response = file_get_contents($url);
-        $data = json_decode($response, true);
+        //Fetch all commodities
+        $commodityResponse = Http::get($this->baseUrl . '/commodities');
 
-        $routes = $data['data'];
+        if (!$commodityResponse->successful()) {
+            return [];
+        }
 
-        return $routes;
+        $commodities = $commodityResponse->json()['data'] ?? [];
+
+        //Take first 10 commodity IDs
+        $commodityIds = collect($commodities)
+            ->take(100)
+            ->pluck('id')
+            ->toArray();
+
+        //Fetch routes in parallel
+        $responses = Http::pool(function ($pool) use ($commodityIds) {
+            return collect($commodityIds)->map(function ($id) use ($pool) {
+                return $pool->get($this->baseUrl . '/commodities_routes', [
+                    'id_commodity' => $id
+                ]);
+            })->toArray();
+        });
+
+        //Merge all results
+        $allRoutes = [];
+
+        foreach ($responses as $response) {
+            if ($response->successful()) {
+                $allRoutes = array_merge($allRoutes, $response->json()['data'] ?? []);
+            }
+        }
+
+        return $allRoutes;
     }
 }
