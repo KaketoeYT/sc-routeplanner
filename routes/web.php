607789ad7de607084ae5dcb1283557440275c035
KaketoeYT <Thijsderogee@gmail.com>
2026-03-17 20:39:34 +0100

    added vehicles

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CommoditiesController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RoutesController;
+use App\Http\Controllers\VehicleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -11,3 +12,5 @@ Route::get('/commodities', [CommoditiesController::class, 'index'])->name('commo
 
 Route::get('/routes', [RoutesController::class, 'index'])->name('routes.index');
 Route::get('/routes/dump', [RoutesController::class, 'dump'])->name('routes.dump');
+
+Route::get('/vehicles', [VehicleController::class, 'index'])->name('vehicles.index');
